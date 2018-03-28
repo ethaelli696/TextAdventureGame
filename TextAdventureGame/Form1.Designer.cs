7f38243f60a2930487d2497c6970c09888376f0f
Ethan Elliott <ethaelli696@ed.amdsb.ca>
2018-03-28 13:32:32 -0400

    made switch statements

--- TextAdventureGame/Form1.Designer.cs
+++ TextAdventureGame/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sceneBox = new System.Windows.Forms.PictureBox();
+            this.tobyBox = new System.Windows.Forms.PictureBox();
+            this.sceneOutput = new System.Windows.Forms.Label();
+            this.errorBox = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tobyBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sceneBox
+            // 
+            this.sceneBox.Location = new System.Drawing.Point(35, 28);
+            this.sceneBox.Name = "sceneBox";
+            this.sceneBox.Size = new System.Drawing.Size(558, 261);
+            this.sceneBox.TabIndex = 0;
+            this.sceneBox.TabStop = false;
+            // 
+            // tobyBox
+            // 
+            this.tobyBox.Image = global::TextAdventureGame.Properties.Resources.toby;
+            this.tobyBox.Location = new System.Drawing.Point(522, 344);
+            this.tobyBox.Name = "tobyBox";
+            this.tobyBox.Size = new System.Drawing.Size(90, 69);
+            this.tobyBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tobyBox.TabIndex = 1;
+            this.tobyBox.TabStop = false;
+            // 
+            // sceneOutput
+            // 
+            this.sceneOutput.AutoSize = true;
+            this.sceneOutput.Location = new System.Drawing.Point(32, 292);
+            this.sceneOutput.Name = "sceneOutput";
+            this.sceneOutput.Size = new System.Drawing.Size(35, 13);
+            this.sceneOutput.TabIndex = 2;
+            this.sceneOutput.Text = "label1";
+            // 
+            // errorBox
+            // 
+            this.errorBox.AutoSize = true;
+            this.errorBox.Location = new System.Drawing.Point(35, 9);
+            this.errorBox.Name = "errorBox";
+            this.errorBox.Size = new System.Drawing.Size(0, 13);
+            this.errorBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(614, 414);
+            this.Controls.Add(this.errorBox);
+            this.Controls.Add(this.sceneOutput);
+            this.Controls.Add(this.tobyBox);
+            this.Controls.Add(this.sceneBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.sceneBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tobyBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox sceneBox;
+        private System.Windows.Forms.PictureBox tobyBox;
+        private System.Windows.Forms.Label sceneOutput;
+        private System.Windows.Forms.Label errorBox;
     }
 }
 
--- TextAdventureGame/Form1.cs
+++ TextAdventureGame/Form1.cs
@@ -16,5 +16,142 @@ namespace TextAdventureGame
         {
             InitializeComponent();
         }
+        int scene;
+        //keypress inputs
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                errorBox.Text = "";
+                //red button (negative)
+                switch (scene)
+                {
+                    case 1:
+                        //scratch the curtains
+                        scene = 2;
+                        break;
+                    case 2:
+                        //norma
+                        scene = 3;
+                        break;
+                    case 4:
+                        //no screw petunia
+                        scene = 5;
+                        break;
+                    case 7:
+                        //naw b
+                        scene = 9;
+                        break;
+                    case 9:
+                        //no dont attack petunia
+                        scene = 11;
+                        break;
+                    case 11:
+                        //go it alone - dont join the feral cats
+                        scene = 24;
+                        break;
+                    case 24:
+                        //no
+                        scene = 23;
+                        break;
+                    case 15:
+                        //dont eat the mouse
+                        scene = 17;
+                        //insert scene here
+                        //then go to scene 18 (death)
+                        break;
+                    case 18:
+                        //no dont knock over petunias stew
+                        scene = 19;
+                        break;
+                    case 19:
+                        //dont eat the petunias
+                        scene = 22;
+                        break;
+                }
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                errorBox.Text = "";
+                switch (scene)
+                {
+                    case 1:
+                        //Go back to sleep
+                        scene = 7;
+                        break;
+                    case 2:
+                        //Five year old Timpothy 
+                        scene = 4;
+                        break;
+                    case 4:
+                        //Go back 
+                        scene = 6;
+                        break;
+                    case 7:
+                        //Eat the treat
+                        scene = 8;
+                        break;
+                    case 9:
+                        //Attack 
+                        scene = 10;
+                        break;
+                    case 11:
+                        //join the feral cats
+                        scene = 26;
+                        break;
+                    case 15:
+                        //eat the mouse 
+                        scene = 16;
+                        break;
+                    case 18:
+                        //stew is spilled
+                        scene = 20;
+                        break;
+                    case 24:
+                        //Fight the dog
+                        scene = 13;
+                        break;
+                    default:
+                        scene = 1;
+                        break;
+                }
+            }
+            else if (e.KeyCode == Keys.B) //blue button
+            {
+                errorBox.Text = "";
+                switch (scene)
+                {
+                    case 1:
+                        //hunt
+                        scene = 15;
+                        break;
+                }
+
+            }
+            else if (e.KeyCode == Keys.N) //yellow button
+            {
+                errorBox.Text = "";
+                switch (scene)
+                {
+                    case 1:
+                        //Make as much noise as you can
+                        scene = 14;
+                        break;
+                }
+            }
+            else
+            {
+                errorBox.Text = "Please use the controls on the left side of the arcade cabinet.";
+            }
+
+            //new switch statement to build the scene
+
+            //another switch statement that displays all scene info
+        }
+        // build the scenes here
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
